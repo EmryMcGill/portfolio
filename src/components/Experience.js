2acb8bdbd0f846c9ec4a35b722aa4cdcdf553d88
Emry <emrymcgill@gmail.com>
2023-12-28 01:02:03 -0500

    worked on the experience cards more

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -5,7 +5,11 @@ const Experience = () => {
         <div className="experience">
             <h1 className="title">Experience</h1>
             <div className="experience-container">
-                <ExperienceCard />
+                <ExperienceCard 
+                company='Columbia Basin Trust' 
+                position='Systems Analyst Intern'
+                desc='a sample description for this job bla bla do do'
+                date='2022-2023' />
             </div>
         </div>
     )
--- src/components/ExperienceCard.js
+++ src/components/ExperienceCard.js
@@ -1,15 +1,21 @@
-const ExperienceCard = ({ company }) => {
+import cbt from '../cbt.png'
+
+const ExperienceCard = ({ company, position, desc, date, location }) => {
     return (
         <div className="experience-card">
-            <div className="left-side">
-                <h1 className="company">{company}</h1>
-                <h2 className="position">{position}</h2>
-                <p className="desc">{desc}</p>
+            
+            <div className="info-container">
+                <div className='title-container'>
+                    <h1 className="position">{position}</h1>
+                    <h2 className="company">{company}</h2>
+                    <p className="desc">{desc}</p>
+                </div>
+                <div className='date-container'>
+                    <h3 className="date">{date}</h3>
+                    <h3 className="location">{location}</h3>
+                </div>
             </div>
-            <div className="right-side">
-                <h3>{date}</h3>
-                <h3>{location}</h3>
-            </div>
+            <img src={cbt}></img>
         </div>
     )
 }   
