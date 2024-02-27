73cb22fe6d2ccebe33b3bdb653c0871c3e325415
Emry <emrymcgill@gmail.com>
2024-02-28 10:54:28 +1300

    fixed some resume info

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -12,25 +12,23 @@ const Experience = () => {
             <h1 className="section-title">Experience</h1>
             <div className="experience-container">
                 <ExperienceCard 
-                company='Teck Resources,'
-                location='Trail, BC'  
-                position='Cathode Utility'
-                desc='a sample description for this job bla bla do do'
-                date='May 2023 - August 2023'
-                img={Tecklogo} />
-                <ExperienceCard 
                 company='Columbia Basin Trust,'
                 location='Castlegar, BC' 
                 position='Systems Analyst Intern'
-                desc='Assisted the broadband team with administrative tasks. Automated tasks, Processed invoices, worked in GIS
-                software to create interactive maps of the broadband network, and composed business emails.'
+                desc='Enhanced efficiency within the broadband team by analyzing 
+                workflows and automating tasks. Utilized Microsoft Power Automate to 
+                streamline invoice processing, identify invoice types, organize them 
+                accurately, and notify the relevant approver via email.'
                 date='May 2022 - August 2022'
                 img={cbt} />
                 <ExperienceCard 
                 company='Greater Trail Community Justice Program Society,'
                 location='Trail, BC'  
                 position='Web Design (volunteer)'
-                desc='Planned and created site design and navigation. Worked in collaboration with the society’s board, including members from the community, RCMP, and local government officials. Incorporated necessary edits and accommodated
+                desc='Planned and created website design and navigation. Worked in 
+                collaboration with the society’s board, including members from the 
+                community, RCMP, and local government officials. Incorporated 
+                necessary edits and accommodated
                 technical concerns.'
                 date='Jan 2020 - June 2020'
                 img={justice} />
@@ -38,18 +36,35 @@ const Experience = () => {
                 company='Rossland Public Library'
                 location='Rossland, BC'  
                 position='Youth Tech Clerk'
-                desc='Assisted and mentored patrons with their technological issues. Repaired computers and setting up e-readers.
-                Listened to concerns and answered questions.'
+                desc='Assisted and mentored patrons with their technological issues, 
+                such as repairing computers and setting up e-readers. Listened to 
+                concerns and answered questions.'
                 date='Feb 2019 - May 2019'
                 img={rossland} />
                 <ExperienceCard 
                 company='Mountain Nugget Chocolate Store'
                 location='Rossland, BC'  
                 position='Retail and Kitchen'
-                desc='Worked in a team setting, provided customer service, handled cash, processed payments, produced chocolates
-                and drinks, cleaned work areas, and trained other staff members.'
+                desc='Collaborated effectively within a dynamic team environment to 
+                deliver exceptional customer service. Responsibilities included
+                processing payments, crafting high-quality chocolates and beverages 
+                and training new staff. As my first job this invaluable experience 
+                served as my introduction to professional life, instilling essential 
+                skills in teamwork, time management, and maintaining high production 
+                standards to consistently exceed customer expectations.'
                 date='Oct 2018 - June 2021'
                 img={nugget} />
+                <ExperienceCard 
+                company='Teck Resources,'
+                location='Trail, BC'  
+                position='Cathode Utility'
+                desc='Contributed to a small team in zinc electrolytic operations. 
+                Managed cathode maintenance, operated forklifts, cranes, and hoists 
+                proficiently, and seamlessly stepping in to cover other operator 
+                responsibilities as required, always with a strong commitment to 
+                ensuring a safe working environment for myself and my team.'
+                date='May 2023 - August 2023'
+                img={Tecklogo} />
             </div>
         </div>
     )
